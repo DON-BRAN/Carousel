ac32fb3332f2640d29fa46e3b4e19b19adb44071
Awa Brandon <brandonawa78@gmail.com>
2024-07-27 19:17:14 +0100

    added an omission

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 document.querySelectorAll(".carousel").forEach(carousel => {
-   const items += carousel.querySelectorAll(".carousel-item")
+   const items = carousel.querySelectorAll(".carousel-item")
    const buttonsHtml = Array.from(items,() =>{
     return    `<span class="carousel-button"></span>`;
    })  
